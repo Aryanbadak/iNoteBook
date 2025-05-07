a728f0097a5e45e69af177301cc88aded132106b
Aryan Badak <aryanbadak10@gmail.com>
2025-05-07 13:23:38 +0530

    create update notes api

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -42,4 +42,31 @@ router.post('/addnotes', [
     }
 })
 
+// update notes
+router.put('/update/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body
+
+    try {
+        const newNotes = {}
+        if (title) { newNotes.title = title }
+        if (description) { newNotes.description = description }
+        if (tag) { newNotes.tag = tag }
+
+        let note = await Notes.findById(req.params.id)
+        if (!note) {
+            return res.status(404).send('Not Found')
+        }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('Not Allowed')
+        }
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNotes }, { new: true })
+        res.json(note)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ error })
+    }
+
+})
+
 module.exports = router
